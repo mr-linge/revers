c078ca1030637bf273da9b7709291b3d4b464e5f
jyzl2020 <2326520660@qq.com>
2023-06-03 15:22:51 +0800

    11

--- push.bat
+++ push.bat
@@ -3,7 +3,7 @@ git config --global user.email "2326520660@qq.com"
 git config --global user.name "jyzl2020"
 git add .
 git commit -m "%commit_msg%"
-git checkout master
-git pull origin master
-git push origin master
+git checkout main
+git pull origin main
+git push origin main
 pause
